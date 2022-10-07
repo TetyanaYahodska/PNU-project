4906d5c7e06d976ef7050b9e2dcca3d88c13438d
Tetiana Yahodska <tyahodska@atlassian.com>
2022-10-07 11:42:39 +0200

    I use the external library

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This project has a library");
+        List<String> list = new ArrayList<>();
+        list.add("qwww");  list.add("qwyy");  list.add("qwyuu");  list.add("qwyutt");  list.add("qwhnjtyueee");
+        System.out.println(StringListHandler.listHandler(list) + "  in this project");
     }
 }
